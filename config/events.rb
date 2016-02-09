d649898b27537751ea2456f49792d1ad675341db
ta-chibana <takuma.chibana314@gmail.com>
2016-02-09 13:25:07 +0900

    Fix settings

--- config/events.rb
+++ config/events.rb
@@ -11,4 +11,7 @@ WebsocketRails::EventMap.describe do
   #     subscribe :new, :to => ProductController, :with_method => :new_product
   #   end
   # The above will handle an event triggered on the client like `product.new`.
+
+  # subscribe :client_connected, to: SocketController, with_method: :connect_user
+  # subscribe :new_message, to: SocketController, with_method: :new_message
 end
